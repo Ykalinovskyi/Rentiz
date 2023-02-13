6552cc1c684bba3a7f7a65b6f392caf65d287792
Evgenyi Kalinovskyi <ykalinovskyi@gmail.com>
2023-02-13 17:53:44 +0300

    finish popular section

--- js/index.js
+++ js/index.js
@@ -31,3 +31,22 @@ if (filter) {
 		})
 	})
 }
+
+//swiper
+const popularSlider = new Swiper('.popular-slider', {
+    spaceBetween: 20,
+    slidesPerView: 1,
+
+    navigation: {
+        nextEl: '.popular-slider-next',
+        prevEl: '.popular-slider-prev'
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+        },
+        660: {
+            slidesPerView: 2
+        }
+    }
+})
